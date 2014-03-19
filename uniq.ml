91e509593dbc07f26f643dc1012a2d81d0e81261
gautamc <gautam.chekuri@gmail.com>
2014-03-19 13:52:36 -0400

    1) Using the `|>` sequencing operator so that order of function calls has better "visibility". 2) Using fold to group list elements into a pipe seperated string.

--- uniq.ml
+++ uniq.ml
@@ -40,28 +40,34 @@ let extract_uniq_field_pair_values ~skip_rows ~master_file ~accum ~field_index ~
 ;;
 
 let extract_uniq_field_grouped_values ~skip_rows ~master_file ~accum ~field_index ~field_to_pair =
-  let sorted_rows = List.sort
-    ~cmp:(fun a b -> compare (List.nth_exn a field_index) (List.nth_exn b field_index))
-    (List.map (List.drop (read_lines master_file) skip_rows) ~f:(fun x -> String.split ~on:'|' x))
-  in
-  let grouped_rows = List.group sorted_rows ~break:(fun a b -> List.nth_exn a field_index <> List.nth_exn b field_index) in
-  let reduced_list = List.map grouped_rows ~f:(
+  List.drop (read_lines master_file) skip_rows
+  |> List.map ~f:(fun x -> String.split ~on:'|' x)
+  |> List.sort ~cmp:(fun a b -> compare (List.nth_exn a field_index) (List.nth_exn b field_index))
+  |> List.group ~break:(fun a b -> List.nth_exn a field_index <> List.nth_exn b field_index)
+  |> List.map ~f:(
     fun group ->
       List.foldi ~init:[] ~f:(
-        fun ixs accum xs ->
-          match ixs > 0 with
-          | false -> xs
+        fun row_ix group_accum row ->
+          match row_ix > 0 with
+          | false -> (List.nth_exn row field_index) :: (List.nth_exn row field_to_pair) :: []
           | true ->
-            if ( List.exists accum ~f:(fun ax -> ax = List.hd_exn (List.tl_exn xs)) ) then
-              accum
+            if ( List.exists ~f:(fun cell -> cell = (List.nth_exn row field_to_pair)) group_accum ) then
+              group_accum
             else
-              accum @ (List.tl_exn xs)
+              group_accum @ (List.nth_exn row field_to_pair) :: []
       ) group
-  ) in
-  if ( List.exists accum ~f:(fun row -> List.hd_exn row = List.hd_exn reduced_list) ) then
-    accum
-  else
-    reduced_list :: accum
+  )
+  |> List.fold ~init:accum ~f:(
+    fun accum grouped_row ->
+      if ( List.exists ~f:(
+        fun accum_row ->
+          (List.nth_exn accum_row 0) = (List.nth_exn grouped_row 0) &&
+          (List.nth_exn accum_row 1) = (List.nth_exn grouped_row 1)
+      ) accum ) then
+        accum
+      else
+        grouped_row :: accum
+  )
 ;;
 
 let rec preorder_listing ~values_list ~field_index root_dir =
@@ -128,11 +134,6 @@ let rec paired_grouped_preorder_listing ~values_list ~field_index ~field_to_pair
       fun accum dir ->
         if( dir <> "data") then
           paired_grouped_preorder_listing (root_dir ^ "/" ^ dir) ~values_list:accum ~field_index:field_index ~field_to_pair:field_to_pair
-          (*
-          List.dedup (List.unordered_append accum (
-            paired_preorder_listing (root_dir ^ "/" ^ dir) ~values_list:accum ~field_index:field_index ~field_to_pair:field_to_pair
-          )) ~compare:(fun a b -> compare (fst a) (fst b))
-          *)
         else
           accum
     )
@@ -145,17 +146,19 @@ let typed_preorder_listing ~field_index ~field_to_pair ~group_p root_dir =
     match field_to_pair with
     | Some field_to_pair ->
       let uniq_field_values = paired_grouped_preorder_listing root_dir ~values_list:[] ~field_index:field_index ~field_to_pair:field_to_pair in
-      List.iter ~f:(
-        fun group ->
-          List.iter ~f:(
-            fun row -> 
-              List.iter ~f:(fun cell -> printf "%s|" cell) row
-          ) group;
-          printf "\n"
-      ) uniq_field_values
+      List.iter uniq_field_values ~f:(
+        fun row ->
+          List.fold ~init:"" ~f:(
+            fun line_accum cell ->
+              if( line_accum <> "" ) then
+                line_accum ^ "|" ^ cell
+              else
+                line_accum ^ cell
+          ) row |> printf "%s\n";
+      )
     | None -> assert false
   )
-  | false ->
+  | false -> (
     match field_to_pair with
     | Some field_to_pair ->
       let uniq_field_values = paired_preorder_listing root_dir ~values_list:[] ~field_index:field_index ~field_to_pair:field_to_pair in
@@ -163,6 +166,7 @@ let typed_preorder_listing ~field_index ~field_to_pair ~group_p root_dir =
     | None ->
       let uniq_field_values = preorder_listing root_dir ~values_list:[] ~field_index:field_index in
       print_str_list uniq_field_values
+  )
 ;;
 
 let command =
