9166df8a6857637e265904235bf34f4282b9a398
gautamc <gautam.chekuri@gmail.com>
2014-03-20 14:17:33 -0400

    using larger test-data input from file

--- t/test2.ml
+++ t/test2.ml
@@ -1,12 +1,15 @@
 open Core.Std;;
+open Core.In_channel;;
 
-[ ["11112" ; "TEST1" ; "FORM1"] ; ["11112" ; "TEST1.1" ; "FORM2"] ; ["11113" ; "TEST2" ; "FORM1"] ; ["11113" ; "TEST2" ; "FORM2"] ; ["11114" ; "TEST3" ; "FORM1"] ]
+List.drop (read_lines "../test_data/master.20140214.idx") 7
+|> List.map ~f:(fun x -> String.split ~on:'|' x)
 |> List.sort ~cmp:(fun a b -> compare (List.nth_exn a 0) (List.nth_exn b 0))
 |> List.group ~break:(fun a b -> (List.nth_exn a 0) <> (List.nth_exn b 0))
 |> List.map ~f:(
   fun group ->
     List.foldi ~init:[] ~f:(
       fun row_ix accum row ->
+
         match row_ix > 0 with
         | false -> (List.nth_exn row 0) :: (List.nth_exn row 1) :: []
         | true ->
@@ -18,6 +21,6 @@ open Core.Std;;
 )
 |> List.iter ~f:(
   fun row ->
-    List.iter ~f:(fun cell -> Printf.printf "%s|" cell) row;    
+    List.iter ~f:(fun cell -> Printf.printf "%s|" cell) row;
     Printf.printf "\n"
 )
