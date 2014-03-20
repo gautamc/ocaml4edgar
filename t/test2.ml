d6429128913791d7eac51ce9a43aef09df8fa32c
gautamc <gautam.chekuri@gmail.com>
2014-03-20 14:42:51 -0400

    Adding a function that can be benchmarked.

--- t/test2.ml
+++ t/test2.ml
@@ -1,26 +1,38 @@
 open Core.Std;;
 open Core.In_channel;;
+open Core_bench.Std;;
 
-List.drop (read_lines "../test_data/master.20140214.idx") 7
-|> List.map ~f:(fun x -> String.split ~on:'|' x)
-|> List.sort ~cmp:(fun a b -> compare (List.nth_exn a 0) (List.nth_exn b 0))
-|> List.group ~break:(fun a b -> (List.nth_exn a 0) <> (List.nth_exn b 0))
-|> List.map ~f:(
-  fun group ->
-    List.foldi ~init:[] ~f:(
-      fun row_ix accum row ->
+let main () =
+  List.drop (read_lines "../test_data/master.20140214.idx") 7
+  |> List.map ~f:(fun x -> String.split ~on:'|' x)
+  |> List.sort ~cmp:(fun a b -> compare (List.nth_exn a 0) (List.nth_exn b 0))
+  |> List.group ~break:(fun a b -> (List.nth_exn a 0) <> (List.nth_exn b 0))
+  |> List.map ~f:(
+    fun group ->
+      List.foldi ~init:[] ~f:(
+        fun row_ix accum row ->
+          match row_ix > 0 with
+          | false -> (List.nth_exn row 0) :: (List.nth_exn row 1) :: []
+          | true ->
+            if ( List.exists ~f:(fun cell -> cell = (List.nth_exn row 1)) accum ) then
+              accum
+            else
+              accum @ (List.nth_exn row 1) :: []
+      ) group
+  )
+  |> List.iter ~f:(
+    fun row ->
+      List.iter ~f:(fun cell -> Printf.printf "%s|" cell) row;
+      Printf.printf "\n"
+  )
+;;
 
-        match row_ix > 0 with
-        | false -> (List.nth_exn row 0) :: (List.nth_exn row 1) :: []
-        | true ->
-          if ( List.exists ~f:(fun cell -> cell = (List.nth_exn row 1)) accum ) then
-            accum
-          else
-            accum @ (List.nth_exn row 1) :: []
-    ) group
-)
-|> List.iter ~f:(
-  fun row ->
-    List.iter ~f:(fun cell -> Printf.printf "%s|" cell) row;
-    Printf.printf "\n"
-)
+let () =
+  Command.run (
+    Bench.make_command [
+      Bench.Test.create ~name:"Test run 1" (
+        fun () ->
+          main ()
+      )
+    ]
+  )
