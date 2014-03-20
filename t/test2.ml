438ae9c3bca6558664444835eeee6b556fd4e972
gautamc <gautam.chekuri@gmail.com>
2014-03-20 17:46:52 -0400

    benchmarking collapse-group (reduce) operation versus the map+sort+group operation.

--- t/test2.ml
+++ t/test2.ml
@@ -2,12 +2,8 @@ open Core.Std;;
 open Core.In_channel;;
 open Core_bench.Std;;
 
-let main () =
-  List.drop (read_lines "../test_data/master.20140214.idx") 7
-  |> List.map ~f:(fun x -> String.split ~on:'|' x)
-  |> List.sort ~cmp:(fun a b -> compare (List.nth_exn a 0) (List.nth_exn b 0))
-  |> List.group ~break:(fun a b -> (List.nth_exn a 0) <> (List.nth_exn b 0))
-  |> List.map ~f:(
+let collapse_groups groups =
+  List.map ~f:(
     fun group ->
       List.foldi ~init:[] ~f:(
         fun row_ix accum row ->
@@ -19,20 +15,49 @@ let main () =
             else
               accum @ (List.nth_exn row 1) :: []
       ) group
-  )
+  ) groups
+;;
+
+let read_sort_group_rows () =
+  List.drop (read_lines "../test_data/master.20140214.idx") 7
+  |> List.map ~f:(fun x -> String.split ~on:'|' x)
+  |> List.sort ~cmp:(fun a b -> compare (List.nth_exn a 0) (List.nth_exn b 0))
+  |> List.group ~break:(fun a b -> (List.nth_exn a 0) <> (List.nth_exn b 0))  
+;;
+
+let main () =
+  read_sort_group_rows ()
+  |> collapse_groups
   |> List.iter ~f:(
     fun row ->
       List.iter ~f:(fun cell -> Printf.printf "%s|" cell) row;
-      Printf.printf "\n"
+      Printf.printf "\n";
   )
 ;;
 
 let () =
+  let records = read_sort_group_rows () in
   Command.run (
     Bench.make_command [
-      Bench.Test.create ~name:"Test run 1" (
+      Bench.Test.create ~name:"Read Sort Group operation" (
         fun () ->
-          main ()
+          ignore (read_sort_group_rows ())
+      ) ;
+      Bench.Test.create ~name:"Collapse Groups operation" (
+        fun () ->
+          ignore (collapse_groups records)
       )
     ]
-  )
+  );
+
+(************* OUTPUT *************
+
+Estimated testing time 20s (2 benchmarks x 10s). Change using -quota SECS.
+┌───────────────────────────┬──────────┬────────────┬──────────┬──────────┬────────────┐
+│ Name                      │ Time/Run │    mWd/Run │ mjWd/Run │ Prom/Run │ Percentage │
+├───────────────────────────┼──────────┼────────────┼──────────┼──────────┼────────────┤
+│ Read Sort Group operation │  34.51ms │ 2_968.19kw │ 869.98kw │ 869.97kw │    100.00% │
+│ Collapse Groups operation │   1.73ms │   315.75kw │  11.11kw │  11.11kw │      5.00% │
+└───────────────────────────┴──────────┴────────────┴──────────┴──────────┴────────────┘
+
+***********************************)
