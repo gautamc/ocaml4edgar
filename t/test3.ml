90ca23c897fe780c0a9dcd4eafcdc42829333ee4
gautamc <gautam.chekuri@gmail.com>
2014-03-22 17:37:44 -0400

    Adding a function to recursively visit each element of a list and call the group-by and merge functions.

--- t/test3.ml
+++ t/test3.ml
@@ -2,7 +2,7 @@ open Core.Std;;
 open Core.In_channel;;
 open Core_bench.Std;;
 
-let sort_group_file ~skip_rows ~master_file ~field_index ~field_to_pair =
+let sort_and_group_file ~skip_rows ~master_file ~field_index ~field_to_pair =
   List.drop (read_lines master_file) skip_rows
   |> List.map ~f:(fun x -> String.split ~on:'|' x)
   |> List.sort ~cmp:(fun a b -> compare (List.nth_exn a field_index) (List.nth_exn b field_index))
@@ -34,8 +34,14 @@ let merge_via_collapsing_same_groups ~accum ~in_list =
 
 let () =
   let master_files = [ ("../test_data/master.20140214.idx", 7); ("../test_data/d1/master", 10); ] in
-  List.iter ~f:(
-    fun pair ->
-      printf "%d" (List.length (sort_group_file ~skip_rows:(snd pair) ~master_file:(fst pair) ~field_index:0 ~field_to_pair:1))
-  ) master_files
-
+  let rec visit_file ~accum ~list_to_visit =
+    match list_to_visit with
+    | pair :: rest ->
+      let rows = sort_and_group_file ~skip_rows:(snd pair) ~master_file:(fst pair) ~field_index:0 ~field_to_pair:1 in
+      visit_file
+        ~accum:(merge_via_collapsing_same_groups ~accum:accum ~in_list:rows)
+        ~list_to_visit:rest
+    | [] -> accum
+  in
+  printf "%d\n" (List.length (visit_file ~accum:[] ~list_to_visit:master_files))
+;;
