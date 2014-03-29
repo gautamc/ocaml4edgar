a5c2e1e7308e8762390955616fd63aa8bb4efd56
gautamc <gautam.chekuri@gmail.com>
2014-03-29 19:13:08 -0400

    Makefile setup for new test program && using the Grouper module in the test programs.

--- t/test4.ml
+++ t/test4.ml
@@ -1,46 +1,12 @@
 open Core.Std;;
 open Core.In_channel;;
+open Grouper;;
 open Core_bench.Std;;
-
-let sort_and_group_file ~skip_rows ~master_file ~field_index ~field_to_pair =
-  List.drop (read_lines master_file) skip_rows
-  |> List.map ~f:(fun x -> String.split ~on:'|' x)
-  |> List.sort ~cmp:(fun a b -> compare (List.nth_exn a field_index) (List.nth_exn b field_index))
-  |> List.group ~break:(fun a b -> List.nth_exn a field_index <> List.nth_exn b field_index)
-  |> List.map ~f:(
-    fun group ->
-      List.foldi ~init:[] ~f:(
-        fun row_ix group_accum row ->
-          match row_ix > 0 with
-          | false -> (List.nth_exn row field_index) :: (List.nth_exn row field_to_pair) :: []
-          | true ->
-            if ( List.exists ~f:(fun cell -> cell = (List.nth_exn row field_to_pair)) group_accum ) then
-              group_accum
-            else
-              group_accum @ (List.nth_exn row field_to_pair) :: []
-      ) group
-  )
-;;
-
-let merge_via_collapsing_same_groups ~accum ~in_list =
-  List.fold ~init:accum ~f:(
-    fun accum grouped_row ->
-      match List.partition_tf ~f:(
-        fun accum_row ->
-          (List.nth_exn accum_row 0) = (List.nth_exn grouped_row 0)
-      ) accum
-      with
-      | ([], _) -> grouped_row :: accum
-      | (row :: [], filtered_accum) ->
-        ((List.hd_exn row) :: ((List.tl_exn row) @ (List.tl_exn grouped_row) |> List.dedup)) :: filtered_accum  
-      | (_,_) -> assert false
-  ) in_list
-;;
 
 let () =
   let master_files = [
+    ("../test_data/master.20140214-1-small.idx", 7); ("../test_data/master.20140214-2-small.idx", 7); ("../test_data/master.20140214-2-small.idx", 7);
     ("../test_data/d2/master", 10); ("../test_data/d1/master", 10); ("../test_data/d1/master", 10);
-    ("../test_data/master.20140214-1-small.idx", 7); ("../test_data/master.20140214-2-small.idx", 7);
   ] in
   let first_pair  = List.nth_exn master_files 0 and
       second_pair = List.nth_exn master_files 1 and
--- t/test5.ml
+++ t/test5.ml
@@ -1,26 +1,7 @@
 open Core.Std;;
 open Core.In_channel;;
+open Grouper;;
 open Core_bench.Std;;
-
-let sort_and_group_file ~skip_rows ~master_file ~field_index ~field_to_pair =
-  List.drop (read_lines master_file) skip_rows
-  |> List.map ~f:(fun x -> String.split ~on:'|' x)
-  |> List.sort ~cmp:(fun a b -> compare (List.nth_exn a field_index) (List.nth_exn b field_index))
-  |> List.group ~break:(fun a b -> List.nth_exn a field_index <> List.nth_exn b field_index)
-  |> List.map ~f:(
-    fun group ->
-      List.foldi ~init:[] ~f:(
-        fun row_ix group_accum row ->
-          match row_ix > 0 with
-          | false -> (List.nth_exn row field_index) :: (List.nth_exn row field_to_pair) :: []
-          | true ->
-            if ( List.exists ~f:(fun cell -> cell = (List.nth_exn row field_to_pair)) group_accum ) then
-              group_accum
-            else
-              group_accum @ (List.nth_exn row field_to_pair) :: []
-      ) group
-  )
-;;
 
 let () =
   let master_files = [
