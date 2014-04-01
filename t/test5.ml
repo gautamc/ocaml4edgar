d670be2d809ec728b717105862c4f9fdeeb602b5
gautamc <gautam.chekuri@gmail.com>
2014-03-31 21:48:20 -0400

    testing with three files

--- t/test5.ml
+++ t/test5.ml
@@ -6,12 +6,16 @@ open Core_bench.Std;;
 let () =
   let master_files = [
     ("../test_data/master.20140214-1-small.idx", 7); ("../test_data/master.20140214-2-small.idx", 7);
+    ("../test_data/master.20140214-2-small.idx", 7);
     ("../test_data/d2/master", 10); ("../test_data/d1/master", 10); ("../test_data/d1/master", 10);
   ] in
   let first_pair  = List.nth_exn master_files 0 and
-      second_pair = List.nth_exn master_files 1
+      second_pair = List.nth_exn master_files 1 and
+      third_pair  = List.nth_exn master_files 2
   in
   let rows_from_first_file  = sort_and_group_file ~skip_rows:(snd first_pair)  ~master_file:(fst first_pair)  ~field_index:0 ~field_to_pair:1 and
-      rows_from_second_file = sort_and_group_file ~skip_rows:(snd second_pair) ~master_file:(fst second_pair) ~field_index:0 ~field_to_pair:1
+      rows_from_second_file = sort_and_group_file ~skip_rows:(snd second_pair) ~master_file:(fst second_pair) ~field_index:0 ~field_to_pair:1 and
+      rows_from_third_file  = sort_and_group_file ~skip_rows:(snd second_pair) ~master_file:(fst third_pair) ~field_index:0 ~field_to_pair:1
+    
   in
-  printf "%d - %d\n" (List.length rows_from_first_file) (List.length rows_from_second_file);
+  printf "%d - %d - %d\n" (List.length rows_from_first_file) (List.length rows_from_second_file) (List.length rows_from_third_file);
