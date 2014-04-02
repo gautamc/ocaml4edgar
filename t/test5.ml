2de88c6d147d234027da4edaec616f0d9f96565c
gautamc <gautam.chekuri@gmail.com>
2014-04-01 23:52:52 -0400

    using Unix.gettimeofday() for find execution time

--- t/test5.ml
+++ t/test5.ml
@@ -1,4 +1,5 @@
 open Core.Std;;
+open Unix;;
 open Core.In_channel;;
 open Grouper;;
 open Core_bench.Std;;
@@ -13,9 +14,10 @@ let () =
       second_pair = List.nth_exn master_files 1 and
       third_pair  = List.nth_exn master_files 2
   in
+  let start_time = Unix.gettimeofday() in
   let rows_from_first_file  = sort_and_group_file ~skip_rows:(snd first_pair)  ~master_file:(fst first_pair)  ~field_index:0 ~field_to_pair:1 and
       rows_from_second_file = sort_and_group_file ~skip_rows:(snd second_pair) ~master_file:(fst second_pair) ~field_index:0 ~field_to_pair:1 and
-      rows_from_third_file  = sort_and_group_file ~skip_rows:(snd second_pair) ~master_file:(fst third_pair) ~field_index:0 ~field_to_pair:1
-    
-  in
+      rows_from_third_file  = sort_and_group_file ~skip_rows:(snd second_pair) ~master_file:(fst third_pair) ~field_index:0 ~field_to_pair:1 in
+  let end_time = Unix.gettimeofday() in
+  printf "%f ; %f : %f\n" start_time end_time (end_time -. start_time);
   printf "%d - %d - %d\n" (List.length rows_from_first_file) (List.length rows_from_second_file) (List.length rows_from_third_file);
