31db51c8257eb96eed3e5a5bbcd7b9d1fa8f4657
gautamc <gautam.chekuri@gmail.com>
2014-04-07 15:34:43 -0400

    Using Core.Std and Core.In_channel's read_lines to read all lines from a file, rather than recursively reading each line and passing it to a callback function.:w

--- calc_size/calc_size.ml
+++ calc_size/calc_size.ml
@@ -1,28 +1,15 @@
 open Sys;;
+open Core.Std;;
+open Core.In_channel;;
 open Printf;;
-open Str;;
 open Unix;;
 
-
-let input_chan = open_in "/data/yacct/edgar/daily-index/2014_01/master.20140110.idx" in
-  let rec process_csv ?ix f =
-    match
-      try Some (input_line input_chan) with
-      | _ -> close_in input_chan; None
-    with
-    | None -> ""
+let all_lines = read_lines "/data/yacct/edgar/daily-index/2014_01/master.20140110.idx" in
+  printf "Total Lines: %d\n" (List.length all_lines);
+  match
+    try (List.nth all_lines 7) with
+    | _ -> None
+  with
+    | None -> assert false
     | Some x ->
-      (
-        let ix = match ix with
-          | None -> 0
-          | Some ix -> ix
-        in
-          f x ix; process_csv ~ix:(ix+1) f
-      )
-  in
-    process_csv (
-      fun x ix ->
-        if ix > 6 && ix < 8 then
-          let fields = Str.split (regexp "|") x in
-            printf "%d - %d\n" ix (List.length fields)
-    )
+      printf "%s\n" x;
