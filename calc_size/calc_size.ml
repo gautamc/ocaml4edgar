5a17c9566d2f7c067ac448bc0faa621f4639a93c
gautamc <gautam.chekuri@gmail.com>
2014-04-08 15:41:12 -0400

    Adding a function that will return as a list all contents of a directory.

--- calc_size/calc_size.ml
+++ calc_size/calc_size.ml
@@ -4,6 +4,26 @@ open Core.In_channel;;
 open Printf;;
 open Unix;;
 
+let ls dir_path_str =
+  match dir_path_str with
+  | "" -> assert false
+  | _ -> (
+    let dir_handle = opendir dir_path_str in
+    let rec dir_reader accum =
+      match
+        try Some (readdir dir_handle) with
+        | End_of_file -> None
+      with
+      | Some x -> dir_reader (x :: accum)
+      | None -> accum
+    in
+      dir_reader []
+  )
+;;
+
+ignore (ls Sys.argv.(1))
+
+(*
 let all_lines = read_lines "/data/yacct/edgar/daily-index/2014_01/master.20140110.idx" in
   printf "Total Lines: %d\n" (List.length all_lines);
   match
@@ -13,3 +33,4 @@ let all_lines = read_lines "/data/yacct/edgar/daily-index/2014_01/master.2014011
     | None -> assert false
     | Some x ->
       printf "%s\n" x;
+      *)
